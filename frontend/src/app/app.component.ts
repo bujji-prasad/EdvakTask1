e59fcc77d6784ba51210ff1e848f93e44745d0d6
bujji-prasad <bpillamgolla@edvak.com>
2025-02-11 07:37:24 +0530

    backend logic added for signup user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { HttpClientModule } from '@angular/common/http';
 // import { LoginComponent } from './components/login/login.component';
 // import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
